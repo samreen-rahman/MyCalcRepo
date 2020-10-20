cdad37e7d30fa2af35a8bd0d8c54da349f79c0b1
samreen-rahman <72907884+samreen-rahman@users.noreply.github.com>
2020-10-20 11:18:17 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -5,8 +5,12 @@ class Calc{
   public static int subs(int a,int b){
     return a-b;
   }
+   public static int prod(int a,int b){
+    return a*b;
+  }
   public static void main(String args[]){
     System.out.println(add(10,20));
     System.out.println(subs(50,20));
+    System.out.println(prod(10,20));
   }
 }
